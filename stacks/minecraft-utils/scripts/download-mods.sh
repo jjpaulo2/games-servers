177ac5a49eb69c7caacb2005218374eb0a9fc3df
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-05-22 20:34:31 -0300

    feat: new dockerfile

--- stacks/minecraft-utils/scripts/download-mods.sh
+++ stacks/minecraft-utils/scripts/download-mods.sh
@@ -2,20 +2,9 @@
 
 set -eou pipefail;
 
-MINECRAFT_VERSION="26.1";
-MOD_LOADER="fabric";
-MODS=(
-    "fabric-api"
-    "xaeros-minimap"
-    "sodium"
-    "sodium-extra"
-    "modmenu"
-    "lithium"
-    "inventory-management"
-    "immediatelyfast"
-    "ferrite-core"
-    "entity-culling"
-);
+MINECRAFT_VERSION=${MINECRAFT_VERSION};
+MOD_LOADER=${MOD_LOADER};
+MODS=${MODS};
 
 PARAM_MOD_LOADER="loaders=[\"$MOD_LOADER\"]";
 PARAM_GAME_VERSION="game_versions=[\"$MINECRAFT_VERSION\"]";
@@ -24,13 +13,13 @@ COLOR_RESET="\033[0m";
 COLOR_GREEN="\033[32m";
 COLOR_RED="\033[31m";
 
-mkdir -p "download/mods";
-rm -f "download/mods/*";
-cd "download/mods";
+mkdir -p "download/$MINECRAFT_VERSION";
+rm -f "download/$MINECRAFT_VERSION**";
+cd "download/$MINECRAFT_VERSION";
 
 echo "[*] Fetching mods for $MOD_LOADER and Minecraft $MINECRAFT_VERSION...";
 
-for mod in "${MODS[@]}"; do
+for mod in $MODS; do
     info_url="https://api.modrinth.com/v2/project/$mod/version";
     json_response=$(curl --get --silent --data-urlencode "$PARAM_MOD_LOADER" --data-urlencode "$PARAM_GAME_VERSION" "$info_url");
     download_url=$(echo "$json_response" | jq -r '.[0].files.[0].url');
@@ -44,5 +33,5 @@ for mod in "${MODS[@]}"; do
     fi;
 done;
 
-zip -r "../Mods $MINECRAFT_VERSION.zip" ./*;
+zip -r "../$MINECRAFT_VERSION.zip" ./*;
 echo -e "[*] Mods have been downloaded and zipped successfully.";
